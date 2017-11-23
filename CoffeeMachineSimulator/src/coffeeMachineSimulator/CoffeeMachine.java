c58b4ebb9f9713ceb51ace80c776761cfb0f1f77
Gabi\Gabriel <gdincagabriel@gmail.com>
2017-11-23 21:40:50 +0200

    Moved run implementation in StateMachine class

--- CoffeeMachineSimulator/src/coffeeMachineSimulator/CoffeeMachine.java
+++ CoffeeMachineSimulator/src/coffeeMachineSimulator/CoffeeMachine.java
@@ -29,39 +29,5 @@ public class CoffeeMachine extends StateMachine {
 		currentState=0;
 	}
 	
-	@Override
-	public void run()
-	{
-		Scanner scanner = new Scanner(System.in);
-		int action;
 
-		while(true)
-		{
-			System.out.println("Credit: $" + currentState);
-			System.out.println("Enter a bill: ");
-			action = scanner.nextInt();
-			if(action == 5 || action == 10)
-			{
-				if(transitionTable.get(currentState).get(action)!=null)
-				{
-					currentState = transitionTable.get(currentState).get(action);
-				}
-				else
-				{
-					System.out.println("\nInvalid option");
-				}
-				if(currentState >= 15)
-				{
-					currentState -=15;
-					System.out.println("\nYou have enough credit for a cofee(price $15). \nYou have bought a coffee ");
-					//break;
-				}
-			}
-			else
-			{
-				System.out.println("\n Enter only $5 and $10 bills");
-			}
-			
-		}
-	}
 }
--- CoffeeMachineSimulator/src/coffeeMachineSimulator/StateMachine.java
+++ CoffeeMachineSimulator/src/coffeeMachineSimulator/StateMachine.java
@@ -2,6 +2,7 @@ package coffeeMachineSimulator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public abstract class StateMachine {
 	int currentState;
@@ -12,8 +13,41 @@ public abstract class StateMachine {
 
 	}
 	
-	public void run() 
-	{}
+	public void run()
+	{
+		Scanner scanner = new Scanner(System.in);
+		int action;
+
+		while(true)
+		{
+			System.out.println("Credit: $" + currentState);
+			System.out.println("Enter a bill: ");
+			action = scanner.nextInt();
+			if(action == 5 || action == 10)
+			{
+				if(transitionTable.get(currentState).get(action)!=null)
+				{
+					Transition(transitionTable.get(currentState).get(action));
+				}
+				else
+				{
+					System.out.println("\nInvalid option");
+				}
+				if(currentState >= 15)
+				{
+					currentState -=15;
+					System.out.println("\nYou have enough credit for a cofee(price $15). \nYou have bought a coffee ");
+					//break;
+				}
+			}
+			else
+			{
+				System.out.println("\n Enter only $5 and $10 bills");
+			}
+			
+		}
+	}
+	
 	
 	
 	public void Transition(int action)
