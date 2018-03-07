2e9268668c6fb79e13f9c580704780ad9411ec98
Gabi\Gabriel <gdincagabriel@gmail.com>
2018-03-07 09:23:10 +0200

    Commited Generator changes

--- RandomNumberGenerator/RandomNumberGenerator/UniformGenerator.cpp
+++ RandomNumberGenerator/RandomNumberGenerator/UniformGenerator.cpp
@@ -4,6 +4,6 @@ float UniformGenerator::Next()
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dis(0, 1.0);
+	std::uniform_real_distribution<> dis(0.0, 1.0);
 	return dis(gen);
 }
--- RandomNumberGenerator/RandomNumberGenerator/main.cpp
+++ RandomNumberGenerator/RandomNumberGenerator/main.cpp
@@ -7,6 +7,7 @@
 
 int main()
 {
+	int counter[10];
 
 	//Middle Square method
 	Generator* midsq = new MiddleSquare(9524);
@@ -16,21 +17,65 @@ int main()
 	Evaluator midsqEvaluator(midsq, 1000);
 
 	PoissonDistribution poisson(uG, 1000, 0.001);
-	BinomialDistribution binomial(uG, 1000, 0.5);
+	BinomialDistribution binomial(uG, 1000, 0.9);
 	float result;
+	
+	for (int i = 0 ; i < 10;i++)
+	{
+		counter[i] = 0;
+	}
+	int j=0;
+	for (int i = 0;i < 100000;i++)
+	{
+		result = uG->Next();
 
-	/*
-	cout << "LCG result: " << endl;
-	result = lcgEvaluator.Evaluate();
-	cout << " " << fabs(result);
-	*/
-	/*cout << endl <<"MiddleSquare result: " << endl;
-	result = midsqEvaluator.Evaluate();
-	cout << " " << fabs(result);
-	*/
+		if (result >= 0.0 && result <= 0.1)
+		{
+			counter[0]++;
+		}
+		else if (result > 0.1 && result <= 0.2)
+		{
+			counter[1]++;
+		}
+		else if (result > 0.2 && result <= 0.3)
+		{
+			counter[2]++;
+		}
+		else if (result > 0.3 && result <= 0.4)
+		{
+			counter[3]++;
+		}
+		else if (result > 0.4 && result <= 0.5)
+		{
+			counter[4]++;
+		}
+		else if (result > 0.5 && result <= 0.6)
+		{
+			counter[5]++;
+		}
+		else if (result > 0.6 && result <= 0.7)
+		{
+			counter[6]++;
+		}
+		else if (result > 0.7 && result <= 0.8)
+		{
+			counter[7]++;
+		}
+		else if (result > 0.8 && result <= 0.9)
+		{
+			counter[8]++;
+		}
+		else if (result > 0.9)
+		{
+			counter[9]++;
+		}
+	}
 
-    cout <<	poisson.Run() << endl;
-	cout << binomial.Run();
+	
+	for (int i = 0;i < 10;i++)
+	{
+		cout << "Intre " << i  << " si " << i +1 << ": " << counter[i] << endl;
+	}
 
 	return 0;
 }
